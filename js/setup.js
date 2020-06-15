c179c7e0773f40c440e4f0a975389895f2c9fa97
Renata <is.ren.id@gmail.com>
2020-06-15 13:29:43 +0300

    Дом задание лекция 4
    Вроде бы сделала, если что забыла, скажи пожалуйста

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,7 @@
 'use strict';
 (function () {
   var userDialog = document.querySelector('.setup');
-  userDialog.classList.remove('hidden');
+  // userDialog.classList.remove('hidden');
   var similarListElement = userDialog.querySelector('.setup-similar-list');
   var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
@@ -11,6 +11,94 @@
   var coatColorRandom = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var eyesColorRandom = ['black', 'red', 'blue', 'yellow', 'green'];
 
+  // Дом задание 4 лекция переменные
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = userDialog.querySelector('.setup-close');
+  var MIN_NAME_LENGTH = 2;
+  var MAX_NAME_LENGTH = 25;
+  var userNameInput = document.querySelector('.setup-user-name');
+  var setupPlayer = userDialog.querySelector('.setup-player');
+  var wizardCoat = setupPlayer.querySelector('.setup-wizard .wizard-coat');
+  var colorWizardCoat = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  var wizardEyes = setupPlayer.querySelector('.setup-wizard .wizard-eyes');
+  var colorWizardEyes = ['black', 'red', 'blue', 'yellow', 'green'];
+  var wizardFireball = setupPlayer.querySelector('.setup-fireball-wrap');
+  var colorWizardFireball = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+  var onPopupEscPress = function (evt) {
+    if ((evt.key === 'Escape') && (document.activeElement !== userNameInput)) {
+      evt.preventDefault();
+      closePopup();
+    }
+  };
+
+  var openPopup = function () {
+    userDialog.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+    // при клике рандомно меняется цвет
+    // пальто
+    wizardCoat.addEventListener('click', function () {
+      var RandomCoatNumber = Math.floor(Math.random() * colorWizardCoat.length);
+      wizardCoat.style.fill = colorWizardCoat[RandomCoatNumber];
+      setupPlayer.querySelector('input[name="coat-color"]').value = colorWizardCoat[RandomCoatNumber];
+    });
+    // глаза
+    wizardEyes.addEventListener('click', function () {
+      var RandomEyesNumber = Math.floor(Math.random() * colorWizardEyes.length);
+      wizardEyes.style.fill = colorWizardEyes[Math.floor(Math.random() * colorWizardEyes.length)];
+      setupPlayer.querySelector('input[name="eyes-color"]').value = colorWizardCoat[RandomEyesNumber];
+    });
+    // fireball
+    wizardFireball.addEventListener('click', function () {
+      var RandomFireballNumber = Math.floor(Math.random() * colorWizardFireball.length);
+      wizardFireball.style.background = colorWizardFireball[RandomFireballNumber];
+      setupPlayer.querySelector('input[name="fireball-color"]').value = colorWizardCoat[RandomFireballNumber];
+    });
+  };
+
+  var closePopup = function () {
+    userDialog.classList.add('hidden');
+    document.removeEventListener('keydown', onPopupEscPress);
+  };
+
+  setupOpen.addEventListener('click', function () {
+    openPopup();
+  });
+
+  setupOpen.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter') {
+      openPopup();
+    }
+  });
+
+  setupClose.addEventListener('click', function () {
+    closePopup();
+  });
+
+
+  // Валидация введенного значения по демо
+
+  userNameInput.addEventListener('invalid', function () {
+    if (userNameInput.validity.valueMissing) {
+      userNameInput.setCustomValidity('Обязательное поле');
+    } else {
+      userNameInput.setCustomValidity('');
+    }
+  });
+
+  userNameInput.addEventListener('input', function () {
+    var valueLength = userNameInput.value.length;
+
+    if (valueLength < MIN_NAME_LENGTH) {
+      userNameInput.setCustomValidity('Ещё ' + (MIN_NAME_LENGTH - valueLength) + ' симв.');
+    } else if (valueLength > MAX_NAME_LENGTH) {
+      userNameInput.setCustomValidity('Удалите лишние ' + (valueLength - MAX_NAME_LENGTH) + ' симв.');
+    } else {
+      userNameInput.setCustomValidity('');
+    }
+  });
+  // конец задания лекция 4
+  // задание 3 лекция вроде
   var getArrRandomIndex = function (arr) {
     return Math.floor((Math.random() * arr.length) + 0);
   };
@@ -34,3 +122,4 @@
 
   userDialog.querySelector('.setup-similar').classList.remove('hidden');
 })();
+
